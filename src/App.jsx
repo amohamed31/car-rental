e6b805734b92be39fb70c10ed1f078dcd619cd03
Ali Mohamed <amohamd31@gmail.com>
2024-07-16 14:03:49 +0300

    navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component, useEffect, useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
-	return <div className='text-3xl font-bold underline'>Hello World</div>;
+	// dark mode start
+	const [theme, setTheme] = useState(
+		localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light'
+	);
+	const element = document.documentElement;
+
+	useEffect(() => {
+		if (theme === 'dark') {
+			element.classList.add('dark');
+			localStorage.setItem('theme', 'dark');
+		} else {
+			element.classList.remove('dark');
+			localStorage.setItem('theme', 'light');
+		}
+	}, [theme]);
+	// dark mode end
+	return (
+		<div>
+			<Navbar
+				theme={theme}
+				setTheme={setTheme}
+			/>
+		</div>
+	);
 };
 
 export default App;
